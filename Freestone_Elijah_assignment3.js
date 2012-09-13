302dbfe180fe898083a5ed5ed183f798aef2045f
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2012-09-12 19:10:18 -0600

    Fixed Object Mutator, added Procedure, myDog Object, and warehouseStore Object (maybe JSON)

--- Freestone_Elijah_assignment3.js
+++ Freestone_Elijah_assignment3.js
@@ -12,11 +12,27 @@ var myCar = { //My Car Object
     security: false
 };
 
+var myDog = { //My Dog Object
+    name: "Echo",
+    breed: "Blue Heeler ",
+    age: 12,
+    attack: true
+};
+
+var warehouseStore = { //Warehouse Store Object (JSON)
+    size: "large",
+    secure: true,
+    exits: 3
+};
+
+myCar.running = false; //Object Mutator
+
 //Global Variables
 var start = "My " + myCar.brand + " backfires with a loud bang as it sputters and dies. Looks like that caught all the Zombies attention! ",
     tries = 3,
     booVar = true,
-    array = ["blocks", "steam"]
+    array = ["blocks", "steam"],
+    danger = "yes"
 ;
 
 var say = function(message) { console.log(message); }; //Console.log Function
@@ -44,8 +60,6 @@ say("I gun it and head straight for the incoming hoard of Zombies. ");
 
 say("I plow through at least 30 of them and speed off towards safety. ");
 
-
-
 var damage = function(array) {
     say("With the Zombies safely in my rearview, I can see " + array[1] + " pouring from under my hood through my trashed widshield");
     say("Dang Zombies broke my radiator!");
@@ -57,89 +71,42 @@ var damage = function(array) {
 
 damage(array);
 
-
-
-myCar.running = function() { //Mutator
-    var no = false
-    return myCar.running
-};
-
-myCar.running(false);
-
-
-/*
-var arrayFunc = function(array) { //Array Function
-    var myArray = ["steam ", "blocks "];
-    for (var i=7, j=array.length; i > j; i--) {
-        say(array[i] + "used to " + myArray[i]);
-    }
-    var arrayOutput = arrayFunc(array);
-    return arrayOutput;
-};
-*/
-//arrayFunc(array);
-
-
 var booFunc = function(booVar1, booVar2) { //Boolean Function
-    var booOut = (booVar1 || booVar2)
-        if (myCar.running && booVar) {
+    var booOut = (booVar1 && booVar2)
+        if (myCar.running || booVar) {
             say("With 2 blocks left, my car makes a horrible screech as the engine seizes. ")
         } else {
             say("My car is still going strong! ")
     } return booOut;
 };
 
-booFunc(true, false);
+booFunc(); //Boolean Output
 
+say("Looks like we're walking from here.");
 
-//arrayFunc(arrayOutput);
+say("We hear a loud crash as we're getting out of the car. ");
 
-//say(arrayOutput);
+say("Zombies are closing in fast! ");
 
+var zombiesComing = function(danger) { //Procedure
+    if (myDog.attack === true) {
+        say(myDog.name + " pounces on a Zombie and rips it to shreds just before it bites me! ")
+    } else {
+        say("Must have been a cat. ")
+    }
+};
 
-
+zombiesComing(danger);
 
 /*
-var zombies = function(zombiesInDrive) { //Procedure
-        if (zombiesInDrive === "yes") { 
-            say("I take out several Zombies like Bowling Pins as I speed down the driveway. ") 
-        } else {
-            say("No Zombies in the driveway. Looks like we're free and clear for the moment. ")
-        }
+var arrayFunc = function(array) { //Array Function
+    var myArray = ["steam ", "blocks "];
+    for (var i=7, j=array.length; i < j; i--) {
+        say(array[i] + "used to " + myArray[i]);
+    }
+    var arrayOutput = arrayFunc(array);
+    return arrayOutput;
 };
 
-var booFunc = function(booVar1, booVar2) { //Boolean Function
-    var booOut = (booVar1 && booVar2)
-        if (booVar1 == booVar2) {
-            say("We need to head west but the road is blocked by an overturned Semi-truck.")
-        } else {
-            say("Roads are clear and we're heading in the right direction! ")
-    } return booOut;
-};
-
-var stringFunc = function(string1, string2) { //String Function
-    var field = "safe";
-        if (field === "fun") {
-            say("Que offroading scene in non-offroading vehicle! ");
-        } else {
-            stringOut = say("Should I take the " + string2 + " around or try cutting through the " + string1 + "? ");
-    } return stringOut;
-};
-
-var blocksLeft = function(blocks) { //Number Function
-    var blocksToGo = 5;
-        say(blocksToGo + " blocks left until downtown. ");
-    while (blocksToGo > 0) {
-            blocksToGo--;
-                if (blocksToGo > 0) {
-                    say(blocksToGo + " more. ");
-                } else {
-                    say("We've reached the edge of downtown. ") };
-            };
-        say("We covered " + blocks + " blocks.");
-    say("Do we cut through downtown of take side streets? ");
-    return blocksToGo;
-};
-
-
+arrayFunc(array);
 */
